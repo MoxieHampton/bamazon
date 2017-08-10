8e90d56ac9e7f91cbd0260c3d84f0a487c33c2fd
Monique Hampton <moniquejhampton@yahoo.com>
2017-08-10 18:04:40 -0400

    establishing SQL

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,24 @@
 var mysql = require('mysql');
 var Table = require('cli-table');
 var inquirer = require('inquirer');
+
+var resString = '';
+var resJSON = '';
+
+var columns = ['item_id', 'product_name','department_name','price','stock_quantity'];
+
+	//connection to SQL
+	var connection = mysql.createConnection({
+		host		:'localHost',
+		user		:'root',
+		password	:'root',
+		port		:'3306',
+		database  	:'bamazon'
+	});
+
+//checking for successful connection
+connection.connect(function(err){
+	if(err) throw err;
+	console.log('connectin id' , connection.threadId);
+	displayTable();
+});
